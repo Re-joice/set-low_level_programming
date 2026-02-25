0ea95d3aed2afb26c14080a3cde6acc261068bb3
Re-Joice <ofosuarmahrejoice@example.com>
2026-02-25 11:58:38 -0800

    Final fix for 0x03-debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * positive_or_negative - prints if integer is positive or negative
- * @i: integer to check
- *
- * Return: Always 0
+ * @i: integer to be checked
  */
-
 void positive_or_negative(int i)
 {
 	if (i > 0)
