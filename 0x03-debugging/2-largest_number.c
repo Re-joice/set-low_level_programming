ab10ccee2be1dfc72fe20d395841b49b3fbaf18d
Re-joice <ofosuarmahrejoice@gmail.com>
2026-02-25 12:54:50 +0000

    Refactor largest_number function for clarity
    Task 2: Fix largest_number logic and style

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,12 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	largest = a;
-
-	if (b > largest)
+	if (a >= b && a >= c)
+		largest = a;
+	else if (b >= a && b >= c)
 		largest = b;
-
-	if (c > largest)
+	else
 		largest = c;
 
 	return (largest);
