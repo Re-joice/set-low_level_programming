536f50339383cf6a39499cf7622ae2197cc6fea6
Re-Joice <ofosuarmahrejoice@example.com>
2026-03-04 03:17:14 -0800

    Fix Task 1: correct prototype and Betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,17 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
-char c;
+	char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int);
+void print_alphabet(void);
 
-#endif /* MAIN_H */
+#endif
