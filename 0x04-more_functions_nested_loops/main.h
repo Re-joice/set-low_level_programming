5e925ac2c7436daea8730c9147160e0fef431d4b
Re-Joice <ofosuarmahrejoice@example.com>
2026-03-05 11:39:45 -0800

    Task 7: function that draws a diagonal line

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
+void print_line(int);
+void print_diagonal(int);
+
+int _putchar(char c);
 
 #endif
