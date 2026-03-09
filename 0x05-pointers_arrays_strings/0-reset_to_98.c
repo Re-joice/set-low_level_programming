00919a545d9d88a56357e98b32be62f5defae247
Re-Joice <ofosuarmahrejoice@example.com>
2026-03-09 10:15:49 -0700

    Task 0: function that updates value to 98 using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates value of pointer to 98
+ * reset_to_98 - updates the value of a pointer to 98
  * @n: pointer to an integer
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
