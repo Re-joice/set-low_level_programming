9c9fd5ba6ad1dc643f1889e34d54178db607e7ea
Re-Joice <ofosuarmahrejoice@example.com>
2026-03-21 02:59:43 -0700

    Add recursive function to calculate string length

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 
 int _putchar(char c);
-void _puts_recursion(char *s);
+
+/* Task 1 */
+void _print_rev_recursion(char *s);
+
+/* Task 2 */
+int _strlen_recursion(char *s);
 
 #endif
