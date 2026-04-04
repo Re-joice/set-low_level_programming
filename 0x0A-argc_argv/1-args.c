fdf1d7c0dee987330b8ed81aa63dfd8ae960cafd
Re-Joice <ofosuarmahrejoice@email.com>
2026-04-04 09:50:18 -0700

    Reverting to argc - 1 with clean Betty style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	(void)argv;
 
 	/* The checker expects the total count including the program name */
-	printf("%d\n", argc);
+	printf("%d\n", argc -1 );
 
 	return (0);
 }
