ee01780b91d83364f2e859bcba11d5cf53e90608
Re-Joice <ofosuarmahrejoice@email.com>
2026-04-04 09:16:36 -0700

    Final submission with logic and void cast

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
+	(void)argv;
+
 	printf("%d\n", argc - 1);
+
 	return (0);
 }
+/* Task 1 */
