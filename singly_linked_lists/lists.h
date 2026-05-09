40d26226e95072b46eac0f3d8ec2679ecf56ab94
Re-joice <ofosuarmahrejoice@example.com>
2026-05-09 06:45:19 -0700

    Create list_len function

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -8,8 +8,6 @@
  * @str: string
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
  */
 
 typedef struct list_s
@@ -20,5 +18,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
