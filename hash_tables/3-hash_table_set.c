bd8d10be1e32a5b16d8ba384bf028347c0566d1c
Re-Joice <ofosuarmahrejoice@gmail.com>
2026-06-19 08:13:27 -0700

    Implement hash table set function

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,58 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	hash_node_t *current;
+	unsigned long int index;
+	char *new_value;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+
+			free(current->value);
+			current->value = new_value;
+			return (1);
+		}
+		current = current->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,6 +2,7 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - node of a hash table
@@ -27,11 +28,41 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size);
 
+/**
+ * hash_djb2 - implements the djb2 algorithm
+ * @str: string to hash
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str);
 
+/**
+ * key_index - gives the index of a key
+ * @key: the key
+ * @size: size of the hash table array
+ *
+ * Return: index where the key/value pair should be stored
+ */
 unsigned long int key_index(const unsigned char *key,
-		unsigned long int size);
+	unsigned long int size);
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key,
+	const char *value);
 
 #endif /* HASH_TABLES_H */
